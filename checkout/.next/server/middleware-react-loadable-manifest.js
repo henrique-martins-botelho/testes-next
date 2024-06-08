92ceb206dc7abe0237d34ce5b05ce104fa12b1ea
Artem Burasov <morti.show@gmail.com>
2024-06-08 18:53:08 +0500

    update

--- checkout/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages\\\\_app.js -> home/nav\":{\"id\":\"pages\\\\_app.js -> home/nav\",\"files\":[]}}"
--- home/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"pages\\\\index.js -> checkout/title\":{\"id\":\"pages\\\\index.js -> checkout/title\",\"files\":[]},\"pages\\\\index.js -> remoteWithButton/button\":{\"id\":\"pages\\\\index.js -> remoteWithButton/button\",\"files\":[]}}"
